575d682229d3b4c38a5376ea9f606e63f5a96727
Erasmus_Crowley <erasmus.crowley@gmail.com>
2013-12-22 17:36:41 -0600

    Getting ready to add compatibility for Project Red.

---
+++ ExNihilo/code/exnihilo/compatibility/ProjectRed.java
@@ -0,0 +1,15 @@
+package exnihilo.compatibility;
+
+import cpw.mods.fml.common.Loader;
+
+public class ProjectRed {
+	public static boolean isLoaded()
+	{
+		return Loader.isModLoaded("ProjectRed");
+	}
+	
+	public static void loadCompatibility()
+	{	
+		
+	}
+}
