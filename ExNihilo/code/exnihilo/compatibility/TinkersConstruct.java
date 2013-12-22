09fb620b39252b3939e2e54688631272e016fc63
Erasmus_Crowley <erasmus.crowley@gmail.com>
2013-12-22 17:35:15 -0600

    Added framework for Tinkers Construct compatibility.

---
+++ ExNihilo/code/exnihilo/compatibility/TinkersConstruct.java
@@ -0,0 +1,19 @@
+package exnihilo.compatibility;
+
+import net.minecraft.block.Block;
+import appeng.api.Materials;
+import cpw.mods.fml.common.Loader;
+import exnihilo.Blocks;
+import exnihilo.registries.SieveRegistry;
+
+public class TinkersConstruct {
+	public static boolean isLoaded()
+	{
+		return Loader.isModLoaded("TinkersConstruct");
+	}
+	
+	public static void loadCompatibility()
+	{	
+		
+	}
+}
