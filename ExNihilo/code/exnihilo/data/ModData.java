010181eb1338d0fe6f4fe1430b33a7528f294e85
Erasmus_Crowley <erasmus.crowley@gmail.com>
2014-02-23 17:25:06 -0600

    Updated version number in preperation for a new release.

--- ExNihilo/code/exnihilo/data/ModData.java
+++ ExNihilo/code/exnihilo/data/ModData.java
@@ -11,7 +11,7 @@ public class ModData {
 	//Metadata
 	public static final String ID = "crowley.skyblock";
 	public static final String NAME = "Ex Nihilo";
-	public static final String VERSION = "1.23";
+	public static final String VERSION = "1.26";
 	
 	public static void setMetadata(ModMetadata data)
 	{
