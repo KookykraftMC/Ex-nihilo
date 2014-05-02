b9d8ef08fd38dc2c6f7d9e78ca040e4dc8fb96ce
Erasmus_Crowley <erasmus.crowley@gmail.com>
2014-05-02 18:10:36 -0500

    Fixed a pair of bugs that where causing the servers to crash on init.

--- src/main/java/exnihilo/network/ChannelHandler.java
+++ src/main/java/exnihilo/network/ChannelHandler.java
@@ -3,11 +3,14 @@ package exnihilo.network;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.network.INetHandler;
 import net.minecraft.network.NetHandlerPlayServer;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.network.FMLIndexedMessageToMessageCodec;
 import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ChannelHandler extends FMLIndexedMessageToMessageCodec<IPacket> {
 
@@ -25,7 +28,7 @@ public class ChannelHandler extends FMLIndexedMessageToMessageCodec<IPacket> {
 		packet.readBytes(data);
 		switch (FMLCommonHandler.instance().getEffectiveSide()) {
 		case CLIENT:
-			packet.executeClient(Minecraft.getMinecraft().thePlayer);
+			packet.executeClient(getClientPlayer());
 			break;
 		case SERVER:
 			INetHandler netHandler = ctx.channel().attr(NetworkRegistry.NET_HANDLER).get();
@@ -33,4 +36,9 @@ public class ChannelHandler extends FMLIndexedMessageToMessageCodec<IPacket> {
 			break;
 		}
 	}
+	
+	@SideOnly(Side.CLIENT)
+    private EntityPlayer getClientPlayer() {
+        return Minecraft.getMinecraft().thePlayer;
+    }
 }
