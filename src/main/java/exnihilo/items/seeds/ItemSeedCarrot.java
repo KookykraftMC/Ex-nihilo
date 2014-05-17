1a810ce35c8ed1724f0da767d4b5759150df00c0
Erasmus_Crowley <erasmus.crowley@gmail.com>
2014-05-17 13:18:36 -0500

    Fixed a few bugs that made it impossible to plant potato or carrot seeds.

--- src/main/java/exnihilo/items/seeds/ItemSeedCarrot.java
+++ src/main/java/exnihilo/items/seeds/ItemSeedCarrot.java
@@ -3,7 +3,6 @@ package exnihilo.items.seeds;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.common.EnumPlantType;
@@ -13,13 +12,13 @@ import exnihilo.data.ModData;
 public class ItemSeedCarrot extends ItemSeedBase{
 
 	public ItemSeedCarrot() {
-		super(Block.getBlockFromItem(Items.carrot), Blocks.dirt);
+		super(Blocks.carrots, Blocks.dirt);
 	}
 	
     @Override
     public Block getPlant(IBlockAccess world, int x, int y, int z)
     {
-        return Block.getBlockFromItem(Items.carrot);
+        return Blocks.carrots;
     }
 
     @Override
